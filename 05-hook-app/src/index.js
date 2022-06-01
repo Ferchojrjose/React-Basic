0226d589e27e35d8c95ddb6bd7f7efef5d0bbbe3
Fernando Serrano <ferchoserrano0@gmail.com>
2022-05-31 19:15:15 -0600

    Empieza hook app

---
+++ 05-hook-app/src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { CounterApp } from './components/01-useState/CounterApp';
+import './index.css'
+// import { HooksApp } from './HooksApp';
+
+ReactDOM.render( <CounterApp />, document.getElementById('root'))
